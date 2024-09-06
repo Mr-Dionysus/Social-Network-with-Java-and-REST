6bef46c7aa996a7ee73722893e358d802f772ae0
Denis <dbreznikov@gmail.com>
2024-09-07 00:33:31 +0300

    feat: add connection to Tomcat with a sample code with a servlet and some jsp

---
+++ src/main/java/org/example/HelloWorldServlet.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter()
+            .write("<h1>Hello, World!</h1>");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello from JSP</title>
+</head>
+<body>
+    <h1>Welcome!</h1>
+    <p>How're you?</p>
+</body>
+</html>
