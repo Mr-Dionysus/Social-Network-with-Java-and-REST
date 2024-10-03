88a5c71f746d0f41ddf43a46a4df49000abe76f9
Denis <dbreznikov@gmail.com>
2024-10-03 21:18:11 +0300

    refactor: replace web.xml config to WebAppInitializer class

---
+++ src/main/java/org/example/WebAppInitializer.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(AppConfig.class);
+
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("springDispatcherServlet", new DispatcherServlet(context));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+}
